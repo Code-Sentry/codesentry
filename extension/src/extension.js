df2ca92526420ebdc824e762398ecd1ffb9facce
Kaio Gerhardt <gerhardt.kaio@gmail.com>
2024-08-22 21:55:17 -0300

    adicionando a verificacao de instalacao na extensao

--- extension/src/extension.js
+++ extension/src/extension.js
@@ -1,8 +1,11 @@
 const vscode = require('vscode');
 const CodeSentryProvider = require('./views/CodeSentryProvider');
 const { registerCommands } = require('./commands');
+const { exec } = require('child_process');
 
-function activate(context) {
+async function activate(context) {
+
+    await isCliToolInstalled();
     // Registrar TreeView
     const treeDataProvider = new CodeSentryProvider();
     const view = vscode.window.createTreeView('codesentryView', {
@@ -15,6 +18,28 @@ function activate(context) {
 }
 
 function deactivate() {}
+
+function isCliToolInstalled(){
+    const commandSystem = "codesentry --version";
+    let isInstalled = false;
+    
+    exec(commandSystem, (error, stdout, stderr) => {
+        const versionRegex = /\b\d+\.\d+\.\d+\b/g;
+        const version = stdout.match(versionRegex);
+
+        if(version != undefined){
+            isInstalled = true;
+            
+            vscode.window.showErrorMessage(`A ferramenta CLI não está instalada corretamente!`, "Instalar").then(selection => {
+                if (selection === "Instalar") {
+                    vscode.env.openExternal(vscode.Uri.parse('https://github.com/KaioGerhardt/codesentry'));
+                }
+            });
+        }
+    });
+
+    return isInstalled;
+}
 
 module.exports = {
     activate,
